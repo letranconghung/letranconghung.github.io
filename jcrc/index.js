4a47e2d65c183e97a2d001e98df6fdde68f47fc3
Le Tran Cong Hung <conghungletran@gmail.com>
2019-07-27 00:51:27 +0800

    improved a lot! complex navbar manipulation in js with jquery!

--- jcrc/index.js
+++ jcrc/index.js
@@ -1,33 +1,34 @@
-// $('#mainNav').fadeIn(2000, ()=> {
-//   console.log("fade in #mainNav successful");
-// });
-// $('#firstPage').fadeIn(2000, ()=> {
-//   console.log('fade in firstPage done');
-// });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-
-// $('#firstPageContent').fadeIn(2000, ()=>{
-//   console.log("fade in firstPageContent successful");
-// });
-
-// $('body').fadeIn(2000, ()=> {
-//   console.log("fade in html successful");
-// }); 
-
-$('#fakebody').fadeIn(2000, ()=> {
-  console.log('fade in #fakebody successful');
-});
-$('#firstPageContent').fadeIn(4000, ()=> {
-  console.log("fade in #firstPageContent successful");
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
 });
 
-$(window).scroll(function(){
-  var scroll = $(window).scrollTop();
-  if(scroll < 600){
-      $('.fixed-top').css('background', 'transparent');
-      $('.navbar-dark .navbar-nav .nav-item .nav-link').css('color', 'rgb(255,255,255)');
-  } else{
-      $('.fixed-top').css('background', '#E7EAED');
-      $('.navbar-dark .navbar-nav .nav-item .nav-link').css('color', 'rgb(0,0,0)');
+window.onscroll = function(){
+  if(window.scrollY >= ($('#mainNav').height()*5)){
+    $('#mainNav').css('backgroundColor', 'rgb(255, 255, 255)');
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').css('color', 'rgba(37, 192, 140, 0.884)');
+    $('.fa-bars').css('color', 'rgba(37, 192, 140, 0.884)');
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').mouseenter((event)=>{
+      event.target.style.color = 'rgba(13, 13, 136, 0.863)';
+    });
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').mouseleave((event)=>{
+      event.target.style.color = 'rgba(37, 192, 140, 0.884)';
+    });
+  }else{
+    $('#mainNav').css('backgroundColor', 'transparent');
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').css('color', 'rgba(194, 194, 204, 0.719)');
+    $('.fa-bars').css('color', 'white');
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').mouseenter((event)=>{
+      event.target.style.color = 'rgba(83, 83, 233, 0.747)';
+    });
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').mouseleave((event)=>{
+      event.target.style.color = 'rgba(194, 194, 204, 0.719)';
+    });
   }
-});
+}
+$('#magicText').fadeIn(2000);
