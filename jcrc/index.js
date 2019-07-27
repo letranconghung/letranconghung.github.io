ffe8c3f19181b0c75cdf00abb14ffebd4e9c23d6
Le Tran Cong Hung <conghungletran@gmail.com>
2019-07-28 00:19:22 +0800

    fix bugs #2

--- jcrc/index.js
+++ jcrc/index.js
@@ -7,10 +7,34 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
   });
 });
-
+function handleWidth(x){
+  if(x.matches){
+    $('#mainNav').css('backgroundColor', 'rgb(255, 255, 255)');
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').css('color', 'rgba(37, 192, 140, 0.884)');
+    $('.fa-bars').css('color', 'rgba(37, 192, 140, 0.884)');
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').mouseenter((event)=>{
+      event.target.style.color = 'rgba(13, 13, 136, 0.863)';
+    });
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').mouseleave((event)=>{
+      event.target.style.color = 'rgba(37, 192, 140, 0.884)';
+    });
+  }else{
+    $('#mainNav').css('backgroundColor', 'transparent');
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').css('color', 'rgba(194, 194, 204, 0.719)');
+    $('.fa-bars').css('color', 'white');
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').mouseenter((event)=>{
+      event.target.style.color = 'rgba(83, 83, 233, 0.747)';
+    });
+    $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').mouseleave((event)=>{
+      event.target.style.color = 'rgba(194, 194, 204, 0.719)';
+    });
+  }
+}
+const x = window.matchMedia("(max-width: 767.98px)");
+handleWidth(x);
+x.addListener(handleWidth);
 window.onscroll = function(){
   if(window.innerWidth >=768){
-    console.log(window.innerWidth);
     if(window.scrollY >= ($('#mainNav').height()*5)){
       $('#mainNav').css('backgroundColor', 'rgb(255, 255, 255)');
       $('#mainNav .navbar-brand,#mainNav div .navbar-nav .nav-item .nav-link').css('color', 'rgba(37, 192, 140, 0.884)');
@@ -32,6 +56,7 @@ window.onscroll = function(){
         event.target.style.color = 'rgba(194, 194, 204, 0.719)';
       });
     }
+    console.log('changed color due to javascript');
   }
 }
 $('#magicText').fadeIn(2000);
