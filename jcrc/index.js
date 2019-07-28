5d67102f8cf6c9b13fce4d31bd7030b9d590c023
Le Tran Cong Hung <conghungletran@gmail.com>
2019-07-28 22:49:20 +0800

    added a lot of members! Using js function to add more members into members section

--- jcrc/index.js
+++ jcrc/index.js
@@ -60,3 +60,42 @@ window.onscroll = function(){
   }
 }
 $('#magicText').fadeIn(2000);
+
+
+const allMembers = ["samuel", "duc", "kevin", "dion", "haoyi", "tiffany", "aron","christopherryan", "dominic", "frederico", "george", "wesley", "ziran", "jason", "haoyan", "jireh", "joseph", "kenny", "louisa", "victoria", "michael", "wenyue", "viroon", "raphael", "jiajia", "yongxian", "timothy", "duy"];
+const messages = ["sammm", "ducc", "kevinnnn", "dionnn", "haoyiiii", "tifffff", "aronn", "ryannn", "dominicccc", "fredddd", "georgeeee", "wesleyyyy", "zirannn", "jasonnn", "haoyannn", "jirehhh", "josephhh", "kennyyy","louisaaaa", "victoriaaaa", "michaelll", "wenyueee", "viroonnn", "raphaelll", "jiajiaaa", "yongxiannn", "timothyyy", "duyyy" ];
+const fullnames = ["Samuel Oscar Yobeliano", "Nguyen Manh Duc", "Kevin Leonardo Theo", "Dion Amarendra Saleh", "Cheah Hao Yi", "Tiffany Irene Prasetio", "Aron Septianto", "Christopher Ryan Wrytzen", "Dominic Bryan", "Frederico Fortino Lim", "Gregorius George Bunjamin", "Ho Jia Cheng Wesley", "Huang Ziran", "Jason Jonathan Tejaputra", "Jiang Haoyan", "Jireh Foo", "Joseph Hengky Dermawan", "Kenny Lewi", "Louisa Wirawan", "Maria Victoria Helena", "Michael Santoso", "Ooi Wen Yue", "Prakhunviroon Sararaksh", "Raphael Tandiono", "Tan Jia Jia", "Tay Yong Xian", "Timothy Putra Prasetio", "Vu Duc Duy"]
+
+
+function addMember (memberid, picturename, pictureformat, position, fullname){
+  let htmlString = '<div class="col-6 col-md-4 col-lg-3 pb-5 flex-column justify-content-start" id="%memberid%"><div class="d-flex justify-content-center align-items-center"><p class="caption"></p><img src="./imgs/%picturename%.%pictureformat%" alt="" class="roundedPic img-fluid"></div><p class="lead text-center pt-3"><em>%position%</em><br>%fullname%</p></div></div>';
+  htmlString = htmlString.replace('%memberid%', memberid);
+  htmlString = htmlString.replace('%picturename%', picturename);
+  htmlString = htmlString.replace('%pictureformat%', pictureformat);
+  htmlString = htmlString.replace('%position%', position);
+  htmlString = htmlString.replace('%fullname%', fullname);
+  console.log(htmlString);
+  document.getElementById('memberlist').insertAdjacentHTML('beforeend', htmlString);
+}
+for(let i = 6; i<allMembers.length;i++){
+  addMember(allMembers[i], 'jcrc logo square', 'jpg', 'random position', fullnames[i]);
+}
+
+
+
+$('.roundedPic').mouseenter((event)=>{
+  event.target.style.opacity = "0.2";
+  const parentid = event.target.parentNode.parentNode.id;
+  const message = messages[allMembers.indexOf(parentid)];
+  const capEl = document.querySelector('#'+parentid+' .caption');
+  const capParentEl = document.querySelector('#' + parentid);
+  capEl.textContent = message;
+});
+$('.roundedPic').mouseleave((event)=>{
+  event.target.style.opacity = "1";
+  const parentid = event.target.parentNode.parentNode.id;
+  const message = messages[allMembers.indexOf(parentid)];
+  const capEl = document.querySelector('#'+parentid+' .caption');
+  const capParentEl = document.querySelector('#' + parentid);
+  capEl.textContent = "";
+});
