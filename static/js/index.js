66ff88f98eaf221154ea794560f0d8acf7e8cc28
Le Tran Cong Hung <conghungletran@gmail.com>
2021-03-21 19:50:31 +0800

    color scheme

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+  $.scrollIt({
+    upKey: 38,             // key code to navigate to the next section
+    downKey: 40,           // key code to navigate to the previous section
+    easing: 'linear',      // the easing function for animation
+    scrollTime: 100,       // how long (in ms) the animation takes
+    activeClass: 'active', // class given to the active nav element
+    onPageChange: null,    // function(pageIndex) that is called when page is changed
+    topOffset: 0           // offste (in px) for fixed top navigation
+  });
+});
